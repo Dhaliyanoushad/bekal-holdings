d4a28d6bd3e57c0af30a5cbfe64bae630c301519
Dhaliya Noushad <daliyadeutsch@gmail.com>
2025-07-19 11:45:57 +0530

    Add initial components and page structure

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+import Test from "@/components/test";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      dools
+      <Test />
+    </>
+  );
+};
+
+export default page;
